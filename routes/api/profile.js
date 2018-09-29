f1fd57da0590f3870df3800dee016822a41efd75
scott <you@example.comitsmescottb123@gmail.com>
2018-09-29 16:41:10 -0400

    profile validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 // Load User Profile
@@ -20,11 +23,13 @@ router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
 router.get(
   "/",
   // authenticate bc it is a protected route
-  passport.authenticate("jwt", { session: false }), 
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
     // findOne is a mongoose function
     Profile.findOne({ user: req.user.id })
+      // get the name and avatar from user model (can do the bc profile belongs to the user)
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user.";
@@ -43,59 +48,64 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    // Get fields
-    const profileFields = {}
+    const { errors, isValid } = validateProfileInput(req.body);
 
-    profileFields.user = req.user.id
-    if (req.body.handle) profileFields.handle = req.body.handle
-    if (req.body.company) profileFields.company = req.body.company
-    if (req.body.website) profileFields.website = req.body.website
-    if (req.body.location) profileFields.location = req.body.location
-    if (req.body.bio) profileFields.bio = req.body.bio
-    if (req.body.status) profileFields.status = req.body.status
-    if (req.body.githubUsername) profileFields.githubUsername = req.body.githubUsername
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    // Get fields
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubUsername)
+      profileFields.githubUsername = req.body.githubUsername;
 
     // Skills - Split into array
-    if (typeof req.body.skills !== 'undefined') {
-      profileFields.skills = req.body.skills.split(',')
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
     }
 
     // Social
-    profileFields.social = {}
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook
-    if (req.body.linkedIn) profileFields.social.linkedIn = req.body.linkedIn
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedIn) profileFields.social.linkedIn = req.body.linkedIn;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update bc profile is found (already created)
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = " That handle already exists";
+            res.status(400).json(errors);
+          }
+
+          //Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
   }
-
-  Profile.findOne({ user: req.user.id })
-  .then(profile => {
-    if (profile) {
-      // Update bc profile is found (already created)
-      Profile.findOneAndUpdate(
-        { user: req.user.id }, 
-        { $set: profileFields }, 
-        { new: true}
-      )
-      .then(profile => res.json(profile))
-    } else {
-      // Create
-
-      // Check if handle exists
-      Profile.findOne({ handle: profileFields.handle})
-      .then(profile => {
-        if (profile) {
-          errors.handle = ' That handle already exists'
-          res.status(400).json(errors)
-        }
-
-        //Save Profile
-        new Profile(profileFields).save()
-        .then(profile => res.json(profile))
-      })
-    }
-  })
 );
 
 module.exports = router;
