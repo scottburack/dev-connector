f15c417154d2a02e9600ef6bfa1aed56487c2b3a
Scott <itsmescottb123@gmail.com>
2019-01-02 17:00:15 -0500

    bug fix

--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -72,7 +72,7 @@ class AddEducation extends Component {
               </Link>
               <h1 className="display-4 text-center">Add Education</h1>
               <p className="lead text-center">
-                Add any school, biitcamp, etc that you have attended.
+                Add any school, bootcamp, etc that you have attended.
               </p>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
