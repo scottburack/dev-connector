8b5cc2b4068feb37eff11cf1e2934401a78197ab
Scott <itsmescottb123@gmail.com>
2019-03-25 20:41:32 -0400

    display, delete comment. clear errors

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import PostItem from "../posts/PostItem";
 import Spinner from "../common/Spinner";
+import CommentForm from "./CommentForm";
 import { getPost } from "../../actions/postActions";
+import CommentFeed from "./CommentFeed";
 
 class Post extends Component {
   componentDidMount = () => {
@@ -21,6 +23,8 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
--- models/Post.js
+++ models/Post.js
@@ -36,7 +36,7 @@ const PostSchema = new Schema({
         required: true
       },
       name: {
-        tpye: String
+        type: String
       },
       avatar: {
         type: String
